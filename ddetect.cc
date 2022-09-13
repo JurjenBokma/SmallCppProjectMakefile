b6bffb5c61d78014531598d6db26e53ba26c809e
Jurjen Bokma <j.bokma@rug.nl>
2022-09-13 11:37:59 +0200

    factor out duplicate code

--- ddetect.cc
+++ ddetect.cc
@@ -2,56 +2,72 @@
 
 #include <fstream>
 #include <iostream>
+#include <limits>
 
 enum {
     ERR_SUCCESS = 0,
     ERR_USAGE,
     ERR_NOT_OPEN,
     ERR_NOT_READ,
+    ERR_COUNTING_FAIL
+};
+
+enum : std::size_t {
+    ERR_NOT_COUNTED = std::numeric_limits<std::size_t>::max()
 };
 
 using namespace std;
 
+int usage(int retval)
+{
+    cerr << R"(
+
+        usage: ddetect <chars> [file1 [file2 [...]]]
+
+)";
+        return retval;
+}
+
+int error(std::string const &msg, int retval)
+{
+    cerr << msg;
+    return retval;
+}
+
+std::size_t count_wanted(std::istream &input, std::string const &wanted)
+{
+    size_t count = 0;
+    while (true)
+    {
+        char ch;
+        input.get(ch);
+        if (!input)
+            break;
+        if (wanted.find(ch) != string::npos)
+            ++count;
+    }
+    if (!input.eof())
+            return ERR_NOT_COUNTED;
+    return count;
+}
+
 int main(int argc, char **argv)
 {
     if (argc <= 1)
-    {
-        cerr << R"(
-
-        usage: ddetect <chars> [file1 [file2 [...]]]
-
-)";
-        return ERR_USAGE;
-    }
+        return usage(ERR_USAGE);
+    
     if (argv[1][1] == 'h')
-    {
-        cerr << R"(
-
-        usage: ddetect <chars> [file1 [file2 [...]]]
-
-)";
-        return ERR_SUCCESS;
-    }
+        return usage(ERR_SUCCESS);
 
     std::string const wanted = argv[1];
 
     if (argc <= 2) // No further arguments, read from stdin.
     {
-        size_t count = 0;
-        while (true)
-        {
-            char ch;
-            cin.get(ch);
-            if (!cin)
-                break;
-            if (wanted.find(ch) != string::npos)
-                ++count;
-        }
-        if (!cin.eof())
-        {
-            cerr << "Error reading from stdin\n";
-            return ERR_NOT_READ;
-        }
+        size_t count = count_wanted(cin, wanted);
+        if (count == ERR_NOT_COUNTED)
+            return error("Could not count wanted chars on stdin.\n",
+                         ERR_COUNTING_FAIL);
+
         cout << "--: " << count << '\n';
     }
 
@@ -63,26 +79,16 @@ int main(int argc, char **argv)
     {
         ifstream input(*arg);
         if (!input)
-        {
-            cerr << "Could not open "
-                 << *arg << " for reading.\n";
-            return ERR_NOT_OPEN;
-        }
-        size_t count = 0;
-        while (true)
-        {
-            char ch;
-            input.get(ch);
-            if (!input)
-                break;
-            if (wanted.find(ch) != string::npos)
-                ++count;
-        }
-        if (!input.eof())
-        {
-            cerr << "Error reading " << *arg  << "\n";
-            return ERR_NOT_READ;
-        }
+            return error("Could not open "s +
+                         *arg + " for reading.\n",
+                         ERR_NOT_OPEN);
+
+        size_t count = count_wanted(input, wanted);
+        if (count == ERR_NOT_COUNTED)
+            return error("Couldn't count wanted chars in "s +
+                         *arg + "\n",
+                         ERR_COUNTING_FAIL);
+
         cout << *arg << ": " << count << '\n';
         total += count;
     }
